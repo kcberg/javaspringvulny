22937ba5c3a6bccce348c4ebb4d814329513bdb0
omaralkhalili <omar.alkhalili@stackhawk.com>
2022-07-26 12:09:06 -0400

    Added route for BitKeeper hidden file 302

--- src/main/java/hawk/controller/HiddenController.java
+++ src/main/java/hawk/controller/HiddenController.java
@@ -1,8 +1,11 @@
 package hawk.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HiddenController {
@@ -17,5 +20,12 @@ public class HiddenController {
     public String jwtAuth(Model model) {
         model.addAttribute("title", "Rando hidden page");
         return "hidden2";
+    }
+
+    @GetMapping("/hidden/BitLocker")
+    @ResponseStatus(code = HttpStatus.FOUND)
+    public String bitLocker(Model model) {
+        model.addAttribute("title", "BitLocker");
+        return "Class Level HTTP Status Overriden. The HTTP Status will be FOUND (CODE 302)";
     }
 }
